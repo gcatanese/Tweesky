8f82886781af7658c3a32ca331107a3e26ff26f3
gcatanese <gcatanese@yahoo.com>
2021-11-17 16:16:34 +0100

    Add JSON, support both Url and HTML body parameters

--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
+from output.json.json_output import get_json
 from src.output.html.html_output import get_html
 from src.parser.parser_factory import get_parser
 
 
-def generate_card(target_url):
+def generate_card(url=None, html=None):
     """
-    Generate Social Media card
-    :param target_url: url
+    Generate Social Media card from a given URL or HTML document
+    :param url: url of the web page
+    :param html: HTML document
     :return: Card object
     """
 
-    parser = get_parser(target_url)
-    parser.fetch()
+    if url is None and html is None:
+        raise Exception("Provide either a URL or an HTML document")
 
+    parser = get_parser(url, html)
+    parser.fetch()
     card = parser.get_card()
 
     return card
@@ -25,7 +29,19 @@ def generate_card_as_html(target_url):
     """
 
     card = generate_card(target_url)
-
     html_card = get_html(card)
 
     return html_card
+
+
+def generate_card_as_json(target_url):
+    """
+    Generate Social Media card
+    :param target_url: url
+    :return: JSON document with OpenGraph and Twitter metadata
+    """
+
+    card = generate_card(target_url)
+    json_card = get_json(card)
+
+    return json_card
